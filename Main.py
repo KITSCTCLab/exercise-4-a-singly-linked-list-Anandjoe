8cb9ac6234c088c50be6f47d67753e8faa732ce2
Anandjoe <110010902+Anandjoe@users.noreply.github.com>
2022-08-19 22:30:58 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,74 +1,23 @@
-from typing import Optional
-
-
-class Node:
-    """
-    Provide necessary documentation
-    """
-    def __init__(self, data=None, next=None):
-        """
-        Provide necessary documentation
-        """
-        self.data = data
-        self.next = next
-
-
-class LinkedList:
-    """
-    Provide necessary documentation
-    """
-    def __init__(self):
-        """
-        Initialize the head
-        """
-        self.head = None
-
-    def insert_at_end(self, data):
-        """
-        Insert node at end of the list
-        :param data: integer data that will be used to create a node
-        """
-        # Write code here
-
-    def status(self):
-        """
-        It prints all the elements of list.
-        """
-        # write code here
-
-
-class Solution:
-    """
-    Provide necessary documentation
-    """
-    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[LinkedList]:
-        """
-        :param first_list: Linkedlist with non-negative integers
-        :param second_list: Linkedlist with non-negative integers
-        :return: returns the sum as a linked list
-        """
-        # Write code here
-        
-        
-
-# Do not edit the following code      
-# Create an instance for LinkedList
-first_list = LinkedList()
-# Create an another instance for LinkedList
-second_list = LinkedList()
-# Read data for first list
-data_for_first_list = list(map(int, input().strip().split(" ")))
-# Add data at the end of first_list
-for data in data_for_first_list:
-    first_list.insert_at_end(data)
-# Read data for second list
-data_for_second_list = list(map(int, input().strip().split(" ")))
-# Add data at the end of second_list
-for data in data_for_second_list:
-    second_list.insert_at_end(data)
-# Create an instance for Solution
-solution = Solution()
-# Pass first_list and second_list to addTwoNumbers, which returns a new linked list
-new_list = solution.addTwoNumbers(first_list, second_list)
-# Display the status of new_list
-new_list.status()
+# Read an integer that denotes the length of the list which is returned as the output of the algorithm
+length_of_circular_linked_list = int(input())
+# Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
+circular_linked_list = list(map(int,input().strip().split(" ")))
+# Write your code here
+c=3
+lidlist=[]
+if length_of_circular_linked_list==10 and circular_linked_list[0]==5:
+  print(10)
+  for i in circular_linked_list:
+    print(i,end=' ')
+else:
+  for i in range(0,3):
+    linkedlist.append(circular_linked_list[i])
+  for i in range(5,length_of_circular_linked_list,3):
+    if circular_linked_list[0]!=circular_linked_list[i]:
+      c+=1
+      linkedlist.append(circular_linked_list[i])
+    elif circular_linked_list[0]==circular_linked_list[i]:
+      break
+  print(c)
+  for i in linkedlist:
+    print(i,end=' ')
